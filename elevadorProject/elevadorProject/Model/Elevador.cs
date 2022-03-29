16557dcd2b1b13a899c9e07bc9b50ba495c30077
Sttefanie Maciel <macielsttefanie@gmail.com>
2022-03-28 21:31:23 -0300

    projeto final c# Ocean

---
+++ elevadorProject/elevadorProject/Model/Elevador.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+
+namespace elevadorProject
+{
+    internal class Elevador
+    {
+        //responsavel pelo andar atual
+        public int AtualAndar;
+        //AndarAtual
+
+        //responsavel por total de pessoas no elevador
+        public int PeopleTotal;
+        //TotalPessoas
+
+        //responsavel pelo total andares
+        public readonly int TotalAndares;
+
+        //responsavel pelo total capacidade max
+        public readonly int TotalCapacidade;
+
+
+        //Iniciar as váriaveis
+        public Elevador(int capacidade, int andares)
+        {
+            TotalAndares = andares;
+            TotalCapacidade = capacidade;
+
+        }
+
+       //Adicionar pessoas
+
+        public void Entrar()
+        {
+            if (PeopleTotal < TotalCapacidade)
+            {
+                PeopleTotal++;
+                string exibaQuantidades = PeopleTotal == 1 ? $"{PeopleTotal} Entrou no elevador." : $"{PeopleTotal} Pessoas entraram no elevador.";
+                Console.WriteLine(exibaQuantidades);
+            }
+            else
+            {
+                Console.WriteLine($"Infelizmente não é possivel ultrapassar a capacidade de {TotalCapacidade} de pessoas no elevador.");
+            }
+        }
+
+
+        public void Sair()
+        {
+           if(PeopleTotal > 0)
+           {
+           PeopleTotal--;
+                Console.WriteLine("Uma pessoa saiu do elevador.");
+           }
+            else
+            {
+               Console.WriteLine("O elevador está vazio.");
+            }
+        }
+        
+
+
+        //Responsavel por subir nos andares 
+        public void Subir()
+        {
+            if(AtualAndar < TotalAndares)
+            {
+                AtualAndar++;
+                string exibaAndarAtual = AtualAndar != 0 ? $"O elevador chegou ao {AtualAndar}ª andar" : $"Descendo para {AtualAndar}ª andar.";
+                Console.WriteLine(exibaAndarAtual);
+            }
+            else
+            {
+                Console.WriteLine($"O  elevador está no {AtualAndar}ª que é o último");
+            }
+        }
+
+        public void Descer()
+        {
+            if(AtualAndar > 0)
+            {
+                AtualAndar--;
+                string exibaAtualAndar = AtualAndar == 0 ? $"O elevador chegou ao térreo" : $"Descendo para o {AtualAndar}ª andar.";
+                    Console.WriteLine(exibaAtualAndar);
+            }
+            else
+            {
+                Console.WriteLine("Elevador está no térreo!");
+            }
+        }
+
+    }
+}
---
+++ elevadorProject/elevadorProject/View/ElevadorView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+
+namespace elevadorProject.View
+{
+    internal class ElevadorView
+    {
+
+        //Exibição Meno de opções
+        public void Projeto(Elevador elevador)
+        {
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.WriteLine($@"Quantidade de andares do edifício: { elevador.TotalAndares } { Environment.NewLine }
+                    Capacidade máxima permitida no elevador ao subir ou descer: { (elevador.TotalCapacidade <= 1 ? elevador.TotalCapacidade + " pessoa" : elevador.TotalCapacidade + " pessoas") } { Environment.NewLine }
+                    Quantidade atual de pessoa (as) no elevador: { (elevador.PeopleTotal <= 1 ? elevador.PeopleTotal + " pessoa" : elevador.PeopleTotal + " pessoas") } { Environment.NewLine }
+                    Andar Atual: { (elevador.AtualAndar == 0 ? "Térreo" : elevador.AtualAndar + "º") } { Environment.NewLine } 
+                    Escolha e informe uma das opções do elevador: { Environment.NewLine }
+                    1 - Entrar no elevador
+                    2 - Sair do elevador
+                    3 - Subir um andar 
+                    4 - Descer um andar 
+                    5 - Finalizar o simulador
+                ");
+
+                var option = Console.ReadLine();
+                Console.Clear();
+
+
+                switch (option)
+                {
+                    case "1": elevador.Entrar(); break;
+                    case "2": elevador.Sair(); break;
+                    case "3": elevador.Subir(); break;
+                    case "4": elevador.Descer(); break;
+                    case "5": continuar = false; break;
+                    default:
+                        Console.WriteLine("Opção não é válida, informe apenas os números das opções."); break;
+                }
+            }
+            //while (continuar); //enquanto for verdadeiro faça.         
+        }
+
+    }
+}
+   
